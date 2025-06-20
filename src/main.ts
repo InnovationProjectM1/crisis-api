d4dbe8ebac12f10c306c7a464d6d8441c27fa55a
Maxime <maxime.boulle@efrei.net>
2025-06-20 09:41:17 +0200

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   
+  const config = new DocumentBuilder()
+    .setTitle('Crisis API')
+    .setDescription('API for Crisis Management System')
+    .setVersion('1.0')
+    .addTag('crisis')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
   // Enable CORS for frontend communication
   app.enableCors({
     origin: ['http://localhost:5173', 'http://localhost:3001'],
